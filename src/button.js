8de0b4c0e3c53abc578942d3bd49ac438fd28921
Muscalu Cristian <muscaiu@yahoo.com>
2017-07-11 16:06:07 +0300

    adding debugger

--- src/button.js
+++ src/button.js
@@ -2,6 +2,7 @@ const Button = {
 	button: '<button id="myButton">Press Me</button>',
 	attachEl: () => {
 		document.getElementById('myButton').addEventListener('click', () => {
+            debugger
 			console.log('Clicked')
 		});
 	}
